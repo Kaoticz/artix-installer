fc0ee71e564581d05d172373179f62aa6706ed05
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2022-01-26 23:57:49 -0300

    Fix typo

--- src/installer.sh
+++ src/installer.sh
@@ -102,6 +102,6 @@ mount $part1 /mnt/boot
 [[ $(grep 'vendor' /proc/cpuinfo) == *"Amd"* ]] && ucode="amd-ucode"
 
 # Install base system and kernel
-basestrap /mnt base base-devel $$my_init elogind-$my_init connman-$my_init $fs_pkgs efibootmgr grub $ucode dhcpcd wpa_supplicant
+basestrap /mnt base base-devel $my_init elogind-$my_init connman-$my_init $fs_pkgs efibootmgr grub $ucode dhcpcd wpa_supplicant
 basestrap /mnt linux-zen linux-firmware linux-zen-headers mkinitcpio
 fstabgen -U /mnt > /mnt/etc/fstab
